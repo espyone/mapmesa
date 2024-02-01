9a247ac0c09cd14df67c391cf574c86d78310707
espyone <42947861+espyone@users.noreply.github.com>
2024-02-01 16:01:09 -0600

    Add files via upload

---
+++ new copy/fuckwithit.js
@@ -0,0 +1,52 @@
+const body = document.body
+const num = document.getElementsByClassName("num")[0]
+const bg1 = document.getElementsByClassName("bg1")[0]
+const bg2 = document.getElementsByClassName("bg2")[0]
+const bg3 = document.getElementsByClassName("bg3")[0]
+const bg4 = document.getElementsByClassName("bg4")[0]
+ body.style.backgroundColor = "steelblue"
+
+
+ bg1.style.backgroundImage = "url(http://mapmesa.com/img/3456NH.jpg)"
+ bg1.style.backgroundColor = "firebrick"
+ bg1.style.backgroundBlendMode = "darken"
+ bg1.style.backgroundSize = "1200px 550px"
+ bg1.style.backgroundRepeat = "repeat"
+ bg1.style.position = "fixed"
+ bg1.style.clipPath = "polygon(0 40%, 100% 40%, 100% 45%, 0 45%"
+ bg1.style.opacity = "1"
+ bg1.style.width = "100vh"
+ bg1.style.height = "100vh"
+ // height:870%;
+ bg2.style.background = "url(http://mapmesa.com/img/3456NH.jpg), linear-gradient(orange,red,purple,black)"
+ bg2.style.backgroundBlendMode = "darken"
+ bg2.style.backgroundSize = "900px 1650px"
+ bg2.style.backgroundRepeat = "repeat"
+ bg2.style.position = "fixed"
+ bg2.style.clipPath = "polygon(0 20%, 100% 20%, 100% 25%, 0 25%"
+ bg2.style.opacity = "1"
+ bg2.style.width = "100vh"
+ bg2.style.height = "100vh"
+ // height:870%;
+ bg3.style.backgroundImage = "url(http://mapmesa.com/img/3456NH.jpg)"
+ bg3.style.backgroundSize = "1700px 1950px"
+ bg3.style.backgroundRepeat = "repeat"
+ bg3.style.backgroundColor = "#2b4221"
+ bg3.style.backgroundBlendMode = "difference"
+ bg3.style.position = "fixed"
+] bg3.style.clipPath = "polygon(0 60%, 100% 60%, 100% 65%, 0 65%"
+ bg3.style.opacity = "1"
+ bg3.style.width = "100vh"
+ bg3.style.height = "100vh"
+ // height:870%;
+ bg4.style.backgroundImage = "url(http://mapmesa.com/img/3456NH.jpg)"
+ bg4.style.backgroundSize = "1200px 550px"
+ bg4.style.backgroundRepeat = "repeat"
+ bg4.style.position = "fixed"
+ bg4.style.backgroundColor = "#6b4221"
+ bg4.style.backgroundBlendMode = "overlay"
+ bg4.style.clipPath = "polygon(0 80%, 100% 80%, 100% 85%, 0 85%"
+ bg4.style.opacity = "1"
+ bg4.style.width = "100vh"
+ bg4.style.height = "100vh"
+ // height:870%;
---
+++ new copy/mess.js
@@ -0,0 +1,131 @@
+const items = [
+
+  {name: '2450 Ashdale', number: '2450', street: 'Ashdale', streetLong: '', gateCSS: '',link:'2450A.html'},
+//  {name: '6800 Austin Center', number: '6800', street: 'Austin Center', streetLong: '', gateCSS: 'gateCode_6800AustinCenter', link:'6800AC.html'},
+  {name: '8210 Bent Tree', number: '8210', street: 'Bent Tree', streetLong: '', gateCSS: '',link:'8210BT.html'},
+  {name: '8405 Bent Tree', number: '8405', street: 'Bent Tree', streetLong: '', gateCSS: '',link:'8405BT.html'},
+  {name: '8524 Burnet', number: '8524', street: 'Burnet', streetLong: '', gateCSS: '',link:'8524B.html'},
+  {name: '6804 North Capital Of Texas', number: '6804', street: '', streetLong: 'North Capital Of Texas', gateCSS: '', link:'6804NC.html'},
+  {name: '7700 North Capital Of Texas', number: '7700', street: '', streetLong: 'North Capital Of Texas', gateCSS: 'gateCode_7700NCapitalOfTX', link:'7700NC.html'},
+  {name: '8501-8559 North Capital Of Texas', number: '8501-8559', streetLong: 'North Capital Of Texas', street: '',gateCSS: '', link:'8500NC.html'},
+//  {name: '9525 North Capital Of Texas', number: '9525', streetLong: 'North Capital Of Texas', street: '', gateCSS: 'gateCode_9525NCapitalOfTX', link:'9525NC.html'},
+  {name: '6500 Champion Grandview', number: '6500', streetLong: 'Champion Grandview', street: '', gateCSS: 'gateCode_6500ChampionGrandview',link:'6500CG.html'},
+  {name: '3839 Dry Creek', number: '3839', street: 'Dry Creek', streetLong: '', gateCSS: '',link:'3839DC.html'},
+  {name: '2819 Foster', number: '2819', street: 'Foster', streetLong: '', gateCSS: '',link:'7744NC.html'},
+  {name: '3500 Greystone', number: '3500', street: 'Greystone', streetLong: '', gateCSS: 'gateCode_3500Greystone', link:'3500G.html'},
+  {name: '3524 Greystone', number: '3524', street: 'Greystone', streetLong: '', gateCSS: '',link:'3524G.html'},
+  {name: '3543 Greystone', number: '3543', street: 'Greystone', streetLong: '', gateCSS: '',link:'3543G.html'},
+ {name: '3600 Greystone', number: '3600', street: 'Greystone', streetLong: '', gateCSS: '',link:'3600G.html'},
+  {name: '3607 Greystone', number: '3607', street: 'Greystone', streetLong: '', gateCSS: '',link:'3543G.html'},
+  {name: '6910 Hart', number: '6910', street: 'Hart', streetLong: '', gateCSS: '',link:'6910H.html'},
+  {name: '7121,7201,7205 Hart', number: '7121,7201,7205', street: 'Hart', streetLong: '', gateCSS: 'gateCode_7205Hart',link:'7201H.html'},
+  {name: '3456 North Hills', number: '3456', street: 'North Hills', streetLong: '', gateCSS: '',link:'3456NH.html'},
+  {name: '3517 North Hills', number: '3517', street: 'North Hills', streetLong: '', gateCSS: '',link:'3517NH.html'},
+  {name: '3600 North Hills', number: '3600', street: 'North Hills', streetLong: '', gateCSS: '',link:'3600NH.html'},
+//  {name: '9226 Jollyville', number: '9226', street: 'Jollyville', streetLong: '', gateCSS: '',link:'9226J.html'},
+  {name: '9500 Jollyville', number: '9500', street: 'Jollyville', streetLong: '', gateCSS: '',link:'9500J.html'},
+  {name: '5811 Mesa', number: '5811', street: 'Mesa', streetLong: '', gateCSS: '',link:'5811M.html'},
+  {name: '8100 North Mopac', number: '8100', street: 'North Mopac', streetLong: '', gateCSS: '',link:'8100NM.html'},
+  {name: '8200 Neely', number: '8200', street: 'Neely', streetLong: '', gateCSS: '',link:'8200N.html'},
+  {name: '7744 Northcross', number: '7744', street: 'Northcross', streetLong: '', gateCSS: '',link:'7744NC.html'},
+  {name: '7685 Northcross', number: '7685', street: 'Northcross', streetLong: '', gateCSS: '',link:'7685N.html'},
+  {name: '5217 Old Spicewood Springs', number: '5217', streetLong: 'Old Spicewood Springs', street: '', gateCSS: '', link:'5217OSS.html'},
+  {name: '2600 Penny', number: '2600', street: 'Penny', streetLong: '', gateCSS: '',link:'2600P.html'},
+  {name: '2601 Penny', number: '2601', street: 'Penny', streetLong: '', gateCSS: '',link:'2701P.html'},
+  {name: '2701 Penny', number: '2701', street: 'Penny', streetLong: '', gateCSS: '',link:'2701P.html'},
+  {name: '8615 Rockwood', number: '8615', street: 'Rockwood', streetLong: '', gateCSS: '',link:'8615R.html'},
+  {name: '7801 Shoal Creek', number: '7801', street: 'Shoal Creek', streetLong: '', gateCSS: '',link:'7801SC.html'},
+//  {name: '3809 Spicewood Springs', number: '3809', street: 'Spicewood Springs', streetLong: '', gateCSS: '',link:'3809SS.html'},
+ {name: '4411 Spicewood Springs', number: '4411', street: 'Spicewood Springs', streetLong: '', gateCSS: 'gateCode_4411SpicewoodSprings', link:'4411SS.html'},
+//  {name: '4711 Spicewood Springs', number: '4711', street: 'Spicewood Springs', streetLong: '', gateCSS: 'gateCode_4711SpicewoodSprings', link:'4711SS.html'},
+  {name: '3605 Steck', number: '3605', street: 'Steck', streetLong: '', gateCSS: 'gateCode_3605Steck', link:'3605S.html'},
+  {name: '4021 Steck', number: '4021', street: 'Steck', streetLong: '', gateCSS: '',link:'4021S.html'},
+  {name: '4159 Steck', number: '4159', street: 'Steck', streetLong: '', gateCSS: '',link:'4159S.html'},
+  {name: '8801-8816 Tallwood', number: '8801-8816', street: 'Tallwood', streetLong: '', gateCSS: '',link:'8800T.html'},
+  {name: '8888 Tallwood', number: '8888', street: 'Tallwood', streetLong: '', gateCSS: 'gateCode_8888Tallwood',link:'8888T.html'},
+  {name: '6805 Wood Hollow', number: '6805', street: 'Wood Hollow', streetLong: '', gateCSS: '',link:'3456NH.html'},
+  {name: '7117 Wood Hollow', number: '7117', street: 'Wood Hollow', streetLong: '', gateCSS: '',link:'7117WH.html'},
+  {name: '7122 Wood Hollow', number: '7122', street: 'Wood Hollow', streetLong: '', gateCSS: 'gateCode_7122WoodHollow', link:'7122WH.html'},
+//  {name: '7131,7201,7303 Wood Hollow', number: '7131,7201,7303', street: 'Wood Hollow', streetLong: '', gateCSS: '',link:'7205H.html'},
+ {name: '7600 Wood Hollow', number: '7600', street: 'Wood Hollow', streetLong: '', gateCSS: '',link:'3600G.html'},
+  {name: '7630 Wood Hollow', number: '7630', street: 'Wood Hollow', streetLong: '', gateCSS: '',link:'7630WH.html'}
+]
+const addresses = document.getElementsByClassName("addresses")[0];
+
+    var arrayLength = items.length;
+    for (var i = 0; i < arrayLength; i++) {
+
+      if (items[i].gateCSS.length > 1 && items[i].streetLong.length > 1) {
+        addresses.innerHTML += '<p class="lSpacing"><span class="aLink">' +
+        '<a href="' + (items[i].link) + '">' + '<span class="aNumber fin">' +
+         (items[i].number) + '</span><span class="aStreet aStLongName fin">' +
+          (items[i].streetLong) + '</span></a><span class="bSpace"></span><span class="newButton '
+         + items[i].gateCSS + '"></span><br></span>'; }
+         else if (items[i].gateCSS.length > 1 && items[i].streetLong.length < 1) {
+           addresses.innerHTML += '<p class="lSpacing"><span class="aLink">' +
+           '<a href="' + (items[i].link) + '">' + '<span class="aNumber fin">' +
+            (items[i].number) + '</span><span class="aStreet fin">' +
+             (items[i].street) + '</span></a><span class="bSpace"></span><span class="newButton '
+            + items[i].gateCSS + '"></span><br></span>'; }
+            else if (items[i].gateCSS.length < 1 && items[i].streetLong.length < 1) {
+              addresses.innerHTML += '<p class="lSpacing"><span class="aLink">' +
+              '<a href="' + (items[i].link) + '">' + '<span class="aNumber fin">' +
+               (items[i].number) + '</span><span class="aStreet fin">' +
+                (items[i].street) + '</span></a><br></span>'; }
+                 else if (items[i].gateCSS.length < 1 && items[i].streetLong.length > 1) {
+                    addresses.innerHTML += '<p class="lSpacing"><span class="aLink">' +
+                    '<a href="' + (items[i].link) + '">' + '<span class="aNumber fin">' +
+                     (items[i].number) + '</span><span class="aStreet aStLongName fin">' +
+                      (items[i].streetLong) + '</span></a><br></span>'; }
+        }
+
+
+        const aNumber = document.getElementsByClassName("aNumber")[0];
+        const aPage = document.getElementsByClassName("aPage")[0];
+        const aLink = document.getElementsByClassName("aLink")[0];
+        const aNoMap = document.getElementsByClassName("aNoMap")[0];
+        const bg = document.getElementsByClassName("bg")[0];
+        const skyle = document.getElementsByClassName("skyle")[0];
+        const newButton = document.getElementsByClassName("newButton")[0];
+
+        var n = Math.floor((Math.random() * 1000) + 2);
+        if (n) {
+          if ( n % 23 === 0  ) {
+          aPage.style.animation = "movearound 2.7s 0.1s 1 normal, bg1 17.3s 4.5s 23 reverse,  flickin2 .3s 6.3s 2 normal,  flickin2 .23s 0.3s 3 normal, font_ani 1.39s 0s 5 normal";
+          addresses.style.animation = "slide_in 2.5s 0s 1 normal, slippy_num 1.9s 2.4s 1 normal";
+          bg.style.animation = "moveit 7.5s 0s 1 normal, moveit 12.5s 12.5s 1 normal";
+          bg.innerHTML += '<img src="large-img/8888T.jpg">'
+          skyle.style.animation = "moveit_sky .55s .8s 3 normal, moveit_sky .45s .5s 2 normal, moveit_sky 1.4s .5s 2 reverse";
+          newButton.style.animation = "slide_in 2.5s 0s 1 normal";
+          }
+         else if (n % 37 === 0  ) {
+          aPage.style.animation = "movearound 2.7s 0.1s 1 normal, bg1 17.3s 4.5s 23 reverse,  flickin2 .3s 6.3s 2 normal,  flickin2 .23s 0.3s 3 normal, font_ani 1.39s 0s 5 normal";
+          addresses.style.animation = "slide_in 2.9s 0s 1 normal, slippy_num 1.9s 2.4s 1 normal";
+         bg.style.animation = "moveit 7.5s 0s 1 normal, moveit 12.5s 12.5s 1 normal";
+        bg.innerHTML += '<img src="large-img/2701P.jpg">'
+          skyle.style.animation = "moveit_sky .55s .8s 3 normal, moveit_sky .45s .5s 2 normal, moveit_sky 1.4s .5s 2 reverse";
+          newButton.style.animation = "slide_in 2.5s 0s 1 normal";
+        } else if (n % 67 === 0  ) {
+        aPage.style.animation = "movearound 2.7s 0.1s 1 normal, bg1 17.3s 4.5s 23 reverse,  flickin2 .3s 6.3s 2 normal,  flickin2 .23s 0.3s 3 normal, font_ani 1.39s 0s 5 normal";
+        addresses.style.animation = "slide_in 3.2s 0s 1 normal, slippy_num 1.5s 2.4s 1 normal";
+        bg.style.animation = "moveit 7.5s 0s 1 normal, moveit 12.5s 12.5s 1 normal";
+      bg.innerHTML += '<img src="large-img/8524B.jpg">';
+        bg.style.mixBlendMode = "screen";
+        skyle.style.animation = "moveit_sky .55s .8s 3 normal, moveit_sky .45s .5s 2 normal, moveit_sky 1.4s .5s 2 reverse";
+        newButton.style.animation = "slide_in 2.5s 0s 1 normal";
+      } else if (n % 3 === 0 ) {
+         bg.innerHTML += '<img src="large-img/3456NH.jpg">'
+       } else if (n % 7 === 0 ) {
+       bg.innerHTML += '<img src="large-img/7122WH.jpg">'
+     } else if (n % 3 <= 1) {
+        bg.innerHTML += '<img src="large-img/3517NH.jpg">';
+          bg.style.mixBlendMode = "overlay";
+        } else {
+           addresses.style.animation = "slide_in 2.9s 0s 1 normal, slippy_num 1.9s 2.4s 1 normal";
+          bg.innerHTML += '<video autoplay muted id="myVideo"> <source src="deer_01.mp4" type="video/mp4">';
+           newButton.style.animation = "slide_in 2.5s 0s 1 normal";
+           bg.style.animation = "moveit 0s 0s 1 normal";
+
+        }
+      }
+
