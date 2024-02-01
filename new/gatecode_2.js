36121f6bd314550960110bfe0bffd05ee14a815e
espyone <42947861+espyone@users.noreply.github.com>
2024-02-01 16:13:42 -0600

    Add files via upload

---
+++ new/gatecode_2.js
@@ -0,0 +1,26 @@
+
+const AC6800 = document.getElementsByClassName("gateCode_6800AustinCenter")[0];
+const NC9525 = document.getElementsByClassName("gateCode_9525NCapitalOfTX")[0];
+const CG6500 = document.getElementsByClassName("gateCode_6500ChampionGrandview")[0];
+const G3500 = document.getElementsByClassName("gateCode_3500Greystone")[0];
+const SS4411 = document.getElementsByClassName("gateCode_4411SpicewoodSprings")[0];
+const SS4711 = document.getElementsByClassName("gateCode_4711SpicewoodSprings")[0];
+const S3605 = document.getElementsByClassName("gateCode_3605Steck")[0];
+const T8888 = document.getElementsByClassName("gateCode_8888Tallwood")[0];
+const WH7122 = document.getElementsByClassName("gateCode_7122WoodHollow")[0];
+const NC7700 = document.getElementsByClassName("gateCode_7700NCapitalOfTX")[0];
+const H7205 = document.getElementsByClassName("gateCode_7205Hart")[0];
+const FW3840 = document.getElementsByClassName("gateCode_3840FarWest")[0];
+
+AC6800.append("#6800");
+NC9525.append("4332");
+CG6500.append("#6500");
+G3500.append("1810");
+SS4411.append("6300");
+SS4711.append("1731");
+S3605.append("1810");
+T8888.append("4283");
+H7205.append("1001");
+WH7122.append("4950");
+NC7700.append("#2016");
+FW3840.append("2671");
